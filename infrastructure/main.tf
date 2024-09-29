39792eb54b25ba6a22fdaf5342f238b76ceca64d
Keerthana B <keerthanabalaji2108@gmail.com>
2024-09-29 16:48:50 +0530

    added terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -14,30 +14,6 @@ provider "aws" {
   region = "eu-west-1"  # Choose your desired region
 }
 
-# Create an IAM Role for EC2 with S3 Read Access
-resource "aws_iam_role" "ec2_role" {
-  name = "EC2S3AccessRole"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-        Effect = "Allow"
-        Sid = ""
-      },
-    ]
-  })
-}
-
-# Attach S3 Read Only Policy to the Role
-resource "aws_iam_role_policy_attachment" "s3_readonly_access" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-  role       = aws_iam_role.ec2_role.name
-}
 
 # Create an S3 bucket for storing the Spring Boot JAR file
 resource "aws_s3_bucket" "expense_lens_bucket_backend" {
@@ -50,6 +26,34 @@ resource "aws_s3_bucket" "expense_lens_bucket_backend" {
 
 }
 
+resource "aws_s3_bucket_public_access_block" "bucket-public-access" {
+    bucket = aws_s3_bucket.expense_lens_bucket_backend.id
+
+    block_public_acls = false
+    block_public_policy = false
+    ignore_public_acls = false
+    restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_website_configuration" "spring-config" {
+  bucket = aws_s3_bucket.expense_lens_bucket_backend.id
+
+  index_document {
+    suffix = "index.html"
+  }
+}
+
+resource "aws_s3_object" "index-html" {
+  bucket = aws_s3_bucket.expense_lens_bucket_backend.bucket
+  key    = "index.html"  # This is the key for the object in S3
+  source = "../index.html"  # Path to the index.html file on your local system
+  acl    = "public-read"  # Make it publicly readable (important for a website)
+
+  # Optional: Set the content-type
+  content_type = "text/html"
+}
+
+
 # Upload the Spring Boot JAR file to the S3 bucket
 resource "aws_s3_object" "jar_file" {
   bucket = aws_s3_bucket.expense_lens_bucket_backend.id  # Use id to refer to the bucket name
@@ -61,6 +65,42 @@ resource "aws_s3_object" "jar_file" {
     Name = "ExpenseLensJAR"
     Environment = "Dev"
   }
+}
+
+
+resource "aws_s3_bucket_ownership_controls" "bucket-ownership" {
+  bucket = aws_s3_bucket.expense_lens_bucket_backend.id
+
+  rule{
+    object_ownership = "BucketOwnerPreferred"
+  }
+
+}
+
+
+resource "aws_s3_bucket_acl" "bucket-acl" {
+  bucket = aws_s3_bucket.expense_lens_bucket_backend.id
+  acl = "public-read"
+
+  depends_on = [ aws_s3_bucket_ownership_controls.bucket-ownership, 
+  aws_s3_bucket_public_access_block.bucket-public-access ]
+}
+
+resource "aws_s3_bucket_policy" "name" {
+  bucket = aws_s3_bucket.expense_lens_bucket_backend.id
+
+   policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "PublicReadGetObject" 
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.expense_lens_bucket_backend.arn}/*"
+      }
+    ]
+  })
 }
 
 # Create a security group for the EC2 instance
@@ -90,31 +130,17 @@ resource "aws_security_group" "springboot_sg" {
   }
 }
 
-# Create an IAM Instance Profile for the EC2 Role
-resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = "EC2S3AccessProfile"  # You can name it anything unique
-  role = aws_iam_role.ec2_role.name
-}
+
 
 # Create an EC2 instance for the Spring Boot application (free tier eligible: t2.micro)
 resource "aws_instance" "expense_lens_backend" {
-  ami           = "ami-0c3a915e3f3aa0e0d"  # Amazon Linux 2 AMI (Free Tier eligible)
+  ami           = "ami-03cc8375791cb8bcf"  
   instance_type = "t2.micro"  # Free tier eligible instance type
   key_name      = "expense-lens-key"
   security_groups = [aws_security_group.springboot_sg.name]
-  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name  # Use the IAM Instance Profile
 
 # Automatically assign a public IP
   associate_public_ip_address = true
-
-  # User data script to install Java, download Spring Boot JAR from S3, and run it
-  user_data = <<-EOF
-              #!/bin/bash
-              sudo yum update -y
-              sudo yum install -y java-17-amazon-corretto  # Install Java 17 (Amazon Linux 2)
-              aws s3 cp s3://${aws_s3_bucket.expense_lens_bucket_backend.bucket}/ExpenseLens-0.0.1-SNAPSHOT.jar /home/ec2-user/ExpenseLens-0.0.1-SNAPSHOT.jar  # Download the Spring Boot JAR from S3
-              nohup java -jar /home/ec2-user/ExpenseLens-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &  # Run the Spring Boot application in the background
-              EOF
 
   tags = {
     Name = "ExpenseLensBackend"
@@ -129,4 +155,15 @@ resource "aws_instance" "expense_lens_backend" {
 output "expense_lens_app_public_ip" {
   value = aws_instance.expense_lens_backend.public_ip
   description = "Public IP to access the Spring Boot ExpenseLens App"
+}
+
+# Output the public IP address to access the Spring Boot app
+output "expense_lens_s3_jar_public_ip" {
+  value = aws_s3_bucket.expense_lens_bucket_backend.website_endpoint
+  description = "Static website hosting of s3 for jar"
+}
+
+variable private_key_path{
+  description = "Path to the SSH private key to be used for authentication"
+  default = "~/.ssh/expense-lens-key"
 }
